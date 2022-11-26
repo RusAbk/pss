471655b2a399710766a1c3d3f1c54ec8d5128cc6
Ruslan <rusabk@yandex.ru>
2022-11-26 04:36:33 +0300

    operating logic moved to pss class

--- main.py
+++ main.py
@@ -1,48 +1,131 @@
 import pygame
-
 pygame.init()
 
+
+
+
 class PSS:
-  def __new__(cls):
+  def __new__(cls, surface, pygame_instance):
     if not hasattr(cls, 'instance'):
         cls.instance = super(PSS, cls).__new__(cls)
     return cls.instance
-  def __init__(self, pygame_instance):
-    self.pygame = pygame_instance
-    self.surface = surface
-    self.components = []
+  def __init__(self, surface, pygame_instance):
+    """
+      Constructor for PSS instance
 
-  def addComponent(self, component):
+      Parameters
+      ---
+      surface
+        Surface to draw components
+      pygame_instance
+        PyGame instance to operate with it
+    """
+    self.pygame_instance = pygame_instance
+    self.components = []
+    self.surface = surface
+    self.default_cursor = pygame.SYSTEM_CURSOR_ARROW
+
+  def addComponent(self, clsName, style = {}):
+    """
+      Creates new component of clsName type
+
+      Parameters
+      ---
+      clsName
+        Classname of component to create
+      props
+        Style properties
+
+      Returns
+      ---
+      object
+        Created component
+    """
+    if isinstance(style, Style):
+      component = clsName(surface=self.surface, pygame_instance=self.pygame_instance, style=style)
+    else:
+      component = clsName(surface=self.surface, pygame_instance=self.pygame_instance, style=Style(style))
     self.components.append(component)
+    return component
 
   def sendEvent(self, e):
+    hover_component = None
+    mouse_moved = False
+    if e.type == self.pygame_instance.MOUSEMOTION:
+      mouse_moved = True
+
     for component in self.components:
       component.sendEvent(e)
+      if mouse_moved and component.rect.collidepoint(e.pos):
+        hover_component = component
+        component.hover()
+      else: 
+        component.unhover()
+
+    if hover_component is None:
+      self.pygame_instance.mouse.set_cursor(self.default_cursor)
+
+
+  def tick(self):
+    for component in self.components:
+      component.tick()
+
+  def draw(self):
+    for component in self.components:
+      component.draw()
+
+  def update(self):
+    self.tick()
+    self.draw()
+
+
+
+
+class Style:
+  def __init__(self, props = {}):
+    self.props = props
+    
 
 class Component:
-  def __init__(self, surface, pygame_instance, left=0, top=0, width=0, height=0):
+  def __init__(self, surface, pygame_instance, left=0, top=0, width=0, height=0, style = Style()):
     self.pygame_instance = pygame_instance
     self.surface = surface
     self.rect = pygame_instance.Rect(left, top, width, height)
     self.clicked = None
+    self.style = style
+    self.cursor = pygame.SYSTEM_CURSOR_ARROW
 
   def sendEvent(self, e):
     if e.type == self.pygame_instance.MOUSEBUTTONDOWN:
       if self.rect.collidepoint(e.pos):
         if self.clicked is not None:
           self.clicked()
-    if e.type == self.pygame_instance.MOUSEMOTION:
-      if self.rect.collidepoint(e.pos):
-        self.pygame_instance.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
 
   def onclick(self, callback):
     self.clicked = callback
 
+  def setStyle(self, style):
+    self.style = dict(style)
+
+  def draw(self):
+    pass
+  def tick(self):
+    pass
+  def hover(self):
+    self.pygame_instance.mouse.set_cursor(self.cursor)
+  def unhover(self):
+    pass
+
+
+
+
 class Button(Component):
-  def __init__(self, surface, pygame_instance, left=0, top=0, width=0, height=0):
-    super().__init__(surface, pygame_instance, left, top, width, height)
+  def __init__(self, surface, pygame_instance, left=0, top=0, width=100, height=40, style = Style()):
+    super().__init__(surface, pygame_instance, left, top, width, height, style)
+    self.cursor = pygame.SYSTEM_CURSOR_HAND 
   
   def draw(self):
+    super().draw()
     self.pygame_instance.draw.rect(self.surface, [100, 100, 100], self.rect)
 
 
@@ -51,23 +134,24 @@ class Button(Component):
 
 def main():
   screen = pygame.display.set_mode([500, 500])
-  b = Button(screen, pygame, 100, 100, 100, 20)
+  pss = PSS(screen, pygame)
+  b = pss.addComponent(Button)
+  b.onclick(quit_game)
 
   app_run = True
   while app_run:
     for e in pygame.event.get():
       if e.type == pygame.QUIT:
         app_run = False
-      b.sendEvent(e)
+      pss.sendEvent(e)
 
     screen.fill([0,0,0])
     pygame.draw.circle(screen, (0, 0, 255), (100, 100), 50)
     
-    b.draw()
-    b.onclick(quit_game)
+    pss.update()
 
     pygame.display.flip()
-    pygame.time.delay(100)
+    pygame.time.delay(30)
 
   pygame.quit()
 
